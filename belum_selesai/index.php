1e0b0a37083984d2f9470dea905155e2bf0905a1
Meiva31 <meiva6678@gmail.com>
2025-05-29 11:16:32 +0700

    Update index.php

--- belum_selesai/index.php
+++ belum_selesai/index.php
@@ -4,18 +4,18 @@ require_once '../helper/connection.php';
 
 $result = mysqli_query($connection, "
   SELECT 
-    transaksi.*, 
+    penjualan.*, 
     pelanggan.nama AS nama_pelanggan, 
     cabang.nama_cabang 
-  FROM transaksi
-  JOIN pelanggan ON transaksi.id_pelanggan = pelanggan.id_pelanggan
-  JOIN cabang ON transaksi.id_cabang = cabang.id_cabang
+  FROM penjualan
+  JOIN pelanggan ON penjualan.id_pelanggan = pelanggan.id_pelanggan
+  JOIN cabang ON penjualan.cabang = cabang.nama_cabang
 ");
 ?>
 
 <section class="section">
   <div class="section-header d-flex justify-content-between">
-    <h1>Data Transaksi Belum Selesai</h1>
+    <h1>Data Transaksi Penjualan</h1>
   </div>
   <div class="row">
     <div class="col-12">
@@ -25,39 +25,33 @@ $result = mysqli_query($connection, "
             <table class="table table-hover table-striped w-100" id="table-1">
               <thead>
                 <tr>
-                  <th>No Transaksi</th>
+                  <th>No Penjualan</th>
                   <th>Nama Pelanggan</th>
                   <th>Cabang</th>
                   <th>Tanggal Masuk</th>
-                  <th>Estimasi Pengambilan</th>
-                  <th>Status</th>
-                  <th>Jenis Pembayaran</th>
+                  <th>Jumlah Pakaian</th>
+                  <th>Status Pembayaran</th>
                   <th>Sub Pembayaran</th>
                   <th>Diskon</th>
+                  <th>Cash</th>
                   <th>Total</th>
                 </tr>
               </thead>
               <tbody>
-                <?php
-                while ($data = mysqli_fetch_array($result)) :
-                ?>
-
+                <?php while ($data = mysqli_fetch_array($result)) : ?>
                   <tr>
-                    <td><?= $data['id_transaksi'] ?></td>
+                    <td><?= $data['id_penjualan'] ?></td>
                     <td><?= $data['nama_pelanggan'] ?></td>
                     <td><?= $data['nama_cabang'] ?></td>
                     <td><?= $data['tanggal_masuk'] ?></td>
-                    <td><?= $data['estimasi_pengambilan'] ?></td>
-                    <td><?= $data['status'] ?></td>
-                    <td><?= $data['jenis_pembayaran'] ?></td>
+                    <td><?= $data['jumlah_pakaian'] ?></td>
+                    <td><?= ucfirst($data['pembayaran']) ?></td>
                     <td><?= $data['sub_pembayaran'] ?></td>
                     <td><?= $data['diskon'] ?></td>
+                    <td><?= $data['cash'] ?></td>
                     <td><?= $data['total'] ?></td>
                   </tr>
-
-                <?php
-                endwhile;
-                ?>
+                <?php endwhile; ?>
               </tbody>
             </table>
           </div>
@@ -69,35 +63,18 @@ $result = mysqli_query($connection, "
 <?php
 require_once '../layout/_bottom.php';
 ?>
-<!-- Page Specific JS File -->
-<?php
-if (isset($_SESSION['info'])) :
-  if ($_SESSION['info']['status'] == 'success') {
-?>
-    <script>
-      iziToast.success({
-        title: 'Sukses',
-        message: `<?= $_SESSION['info']['message'] ?>`,
-        position: 'topCenter',
-        timeout: 5000
-      });
-    </script>
-  <?php
-  } else {
-  ?>
-    <script>
-      iziToast.error({
-        title: 'Gagal',
-        message: `<?= $_SESSION['info']['message'] ?>`,
-        timeout: 5000,
-        position: 'topCenter'
-      });
-    </script>
-<?php
-  }
 
-  unset($_SESSION['info']);
-  $_SESSION['info'] = null;
-endif;
-?>
+<!-- Page Specific JS File -->
+<?php if (isset($_SESSION['info'])) : ?>
+  <script>
+    iziToast.<?= $_SESSION['info']['status'] === 'success' ? 'success' : 'error' ?>({
+      title: '<?= $_SESSION['info']['status'] === 'success' ? 'Sukses' : 'Gagal' ?>',
+      message: `<?= $_SESSION['info']['message'] ?>`,
+      position: 'topCenter',
+      timeout: 5000
+    });
+  </script>
+  <?php unset($_SESSION['info']); ?>
+<?php endif; ?>
+
 <script src="../assets/js/page/modules-datatables.js"></script>
